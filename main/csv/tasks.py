45c63ca865405abe5455975b7b8f409515433caa
Orxan <anvarliorxan@gmail.com>
2021-03-20 01:22:17 +0400

    second commit

--- main/csv/tasks.py
+++ main/csv/tasks.py
@@ -22,8 +22,8 @@ def generate_data_to_csv_celery_task(id):
     csv_file = ContentFile(csv_buffer.getvalue().encode('utf-8'))
 
     file = CSVFiles()
-    file.user = c.user
-    file.csv_data = c
+    file.user = csv_data.user
+    file.csv_data = csv_data
     file.save()
     file.file.save('output.csv', csv_file)
 
